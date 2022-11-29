f13df9a3b76c6cb753485274cf791a78aa4e758f
sll0ne <d-nagaev90@bk.ru>
2022-11-29 12:08:23 +0300

    Task05

---
+++ Task05/guess_number/bin/guess-number.php
@@ -0,0 +1,13 @@
+<?php
+
+    $autoloadGit = __DIR__ . '/../vendor/autoload.php';
+    $autoloadPackagist = __DIR__ . '/../../../autoload.php';
+
+if (file_exists($autoloadGit)) {
+    require_once($autoloadGit);
+} else {
+    require_once($autoloadPackagist);
+}
+    use function nagaevdg\guess_number\Controller\startGame;
+
+    startGame();
